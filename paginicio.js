35b8c64ab1d9783cbf73fa716d3a4bd46989f38d
Alextheprogrammer0 <alejandrofloreselizarraras@gmail.com>
2024-10-04 00:00:51 -0600

    Add files via upload

---
+++ paginicio.js
@@ -0,0 +1,59 @@
+
+/*crear variables
+
+var fondo
+fondo = createSprite(500,500,250,250);
+
+function preload(){
+    fondo = loadAnimation("descargar(1).jpeg");
+ }
+
+
+
+ drawSprites();
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
